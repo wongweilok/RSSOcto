b556e3c44fdad29812530206fafabf776d044058
Wong Wei Lok <wongweilok@disroot.org>
2021-10-13 23:04:59 +0800

    feat: Add a seperate ViewModel for AddFeedFragment

--- app/src/main/java/com/weilok/rssocto/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/weilok/rssocto/viewmodel/FeedViewModel.kt
@@ -43,73 +43,9 @@ class FeedViewModel @Inject constructor(
     val feeds = repo.localFeeds
     val entries = repo.localEntries
 
-    @Bindable
-    val inputUrl = MutableLiveData<String>()
-
-    @Bindable
-    val urlValidation = MutableLiveData<String>()
-
-    @Bindable
-    val feedType = MutableLiveData<String>()
-
-    // Get user input Feed URL and request for XML data
-    fun getFeed() {
-        val url = inputUrl.value!!
-
-        when (feedType.value!!) {
-            "RSS" -> fetchRssFeed(url)
-            "ATOM" -> fetchAtomFeed(url)
-        }
-
-        inputUrl.value = ""
-    }
-
-    private fun fetchAtomFeed(url: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            repo.fetchAtomFeed(url)
-        }
-    }
-
-    private fun fetchRssFeed(url: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            repo.fetchRssFeed(url)
-        }
-    }
-
     override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
     }
 
     override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
-    }
-
-    // Fetch Feed format for URL validation
-    private var timer = Timer()
-    private val delay: Long = 1000
-    private val client = OkHttpClient()
-
-    val isUrlValid = ObservableBoolean(false)
-    val isFeedExist = ObservableBoolean(false)
-
-    fun onTextChanged() {
-        timer.cancel()
-        isUrlValid.set(false)
-    }
-
-    fun afterTextChanged() {
-        timer = Timer()
-        timer.schedule(object : TimerTask() {
-            override fun run() {
-                viewModelScope.launch(Dispatchers.IO) {
-                    repo.fetchFeedType(
-                        inputUrl.value!!,
-                        client,
-                        urlValidation,
-                        feedType,
-                        isUrlValid,
-                        isFeedExist
-                    )
-                }
-            }
-        }, delay)
     }
 }
