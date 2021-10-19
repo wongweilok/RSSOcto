74ef1086cf8d9d03880d648a13731f3b1d16c56c
Wong Wei Lok <contact.weilok@skiff.com>
2021-10-19 20:40:30 +0800

    chore: Add license header and minor changes

--- app/src/main/java/com/weilok/rssocto/viewmodels/EntryContentViewModel.kt
+++ app/src/main/java/com/weilok/rssocto/viewmodels/EntryContentViewModel.kt
@@ -1,10 +1,30 @@
+/*
+    Copyright (C) 2021 Wong Wei Lok <wongweilok@disroot.org>
+
+    This file is part of RSSOcto
+
+    RSSOcto is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RSSOcto is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this RSSOcto.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package com.weilok.rssocto.viewmodels
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.weilok.rssocto.data.local.entities.Entry
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+
+import com.weilok.rssocto.data.local.entities.Entry
 
 @HiltViewModel
 class EntryContentViewModel @Inject constructor(
